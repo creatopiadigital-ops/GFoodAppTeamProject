b56c066eb8ec4c648012c838bf75f6059b9c0786
Vincent081724 <vincentleyson24@gmail.com>
2026-02-26 22:59:34 +0800

    Menu Update for Menulist

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,162 +1,337 @@
+//Meals
 import Menu1 from '../images/Chicken.png'
 import Menu2 from '../images/Karekare.png'
 import Menu3 from '../images/CrispyPata.png'
 import Menu4 from '../images/LechonKawali.png'
 import Menu5 from '../images/Chicken.png'
 import Menu6 from '../images/BistikTagalog.png'
-import Menu7 from '../images/Chicken.png'
-import Menu8 from '../images/Karekare.png'
-import Menu9 from '../images/CrispyPata.png'
-import Menu10 from '../images/LechonKawali.png'
-import Menu11 from '../images/Chicken.png'
-import Menu12 from '../images/BistikTagalog.png'
-import Menu13 from '../images/Chicken.png'
-import Menu14 from '../images/Karekare.png'
-import Menu15 from '../images/CrispyPata.png'
-import Menu16 from '../images/LechonKawali.png'
-import Menu17 from '../images/LechonKawali.png'
-import Menu18 from '../images/Chicken.png'
-import Menu19 from '../images/BistikTagalog.png'
-import Menu20 from '../images/Chicken.png'
-import Menu21 from '../images/Karekare.png'
-import Menu22 from '../images/CrispyPata.png'
-import Menu23 from '../images/LechonKawali.png'
-import Menu24 from '../images/Chicken.png'
-import Menu25 from '../images/BistikTagalog.png'
-import Menu26 from '../images/Karekare.png'
-import Menu27 from '../images/CrispyPata.png'
-import Menu28 from '../images/LechonKawali.png'
-import Menu29 from '../images/Chicken.png'
-import Menu30 from '../images/BistikTagalog.png'
+//Seafood
+import Menu7 from '../images/GrilledBangus.png'
+import Menu8 from '../images/MixesSeafood.png'
+import Menu9 from '../images/SweatSour.png'
+import Menu10 from '../images/ButterGarlic.png'
+import Menu11 from '../images/Calamares.png'
+// Dessert
+import Menu12 from '../images/LechePlan.png'
+import Menu13 from '../images/Halohalo.png'
+//Sizzlings
+import Menu14 from '../images/Porksisig.png'
+import Menu15 from '../images/Beefsisig.png'
+import Menu16 from '../images/Chickensisig.png'
+//Beverages
+import Menu17 from '../images/Cucumberjuice.png'
+import Menu18 from '../images/Icetea.png'
+import Menu19 from '../images/Calamansijuices.png'
+import Menu20 from '../images/Coke.png'
+import Menu21 from '../images/Sprite.png'
+import Menu22 from '../images/Bottledwater.png'
+//Samgyusal
+import Menu23 from '../images/Porksamgyup.png'
+import Menu24 from '../images/Beefsamgyup.png'
+//Add Ons
+import Menu25 from '../images/Plainrice.png'
+import Menu26 from '../images/Garlicrice.png'
+import Menu27 from '../images/Kimchi.png'
+import Menu28 from '../images/Cheesedip.png'
+import Menu29 from '../images/Kimchi.png'
+import Menu30 from '../images/Cheesedip.png'
+import React, { useState } from 'react'
+
 const Menu = () => {
   const MenuItems = [
+    //Meals
+    { id: 1, menuImage: Menu1, menuTitle: 'Adobo', menuCategory: 'Meals', menuPrice: '$380' },
+    { id: 2, menuImage: Menu2, menuTitle: 'Kare-kare', menuCategory: 'Meals', menuPrice: '$550' },
+    { id: 3, menuImage: Menu3, menuTitle: 'Crispy Pata', menuCategory: 'Meals', menuPrice: '$720' },
     {
-      menuImage: Menu1,
-      menuTitle: 'Adobo',
+      id: 4,
+      menuImage: Menu4,
+      menuTitle: 'Lechon Kawali',
       menuCategory: 'Meals',
-      menuPrice: '$380'
+      menuPrice: '$480'
     },
-    { menuImage: Menu2, menuTitle: 'Kare-kare', menuCategory: 'Meals', menuPrice: '$550' },
-    { menuImage: Menu3, menuTitle: 'Crispy Pata', menuCategory: 'Meals', menuPrice: '$720' },
-    { menuImage: Menu4, menuTitle: 'Lechon Kawali', menuCategory: 'Meals', menuPrice: '$480' },
-    { menuImage: Menu5, menuTitle: 'Chicken Inasal', menuCategory: 'Meals', menuPrice: '$420' },
-    { menuImage: Menu6, menuTitle: 'Bistik Tagalog', menuCategory: 'Meals', menuPrice: '$420' },
-    { menuImage: Menu7, menuTitle: 'Grilled Bangus', menuCategory: 'Seafood', menuPrice: '$460' },
-    { menuImage: Menu8, menuTitle: 'Mixed Seafood', menuCategory: 'Seafood', menuPrice: '$520' },
     {
+      id: 5,
+      menuImage: Menu5,
+      menuTitle: 'Chicken Inasal',
+      menuCategory: 'Meals',
+      menuPrice: '$420'
+    },
+    {
+      id: 6,
+      menuImage: Menu6,
+      menuTitle: 'Bistik Tagalog',
+      menuCategory: 'Meals',
+      menuPrice: '$420'
+    },
+    //Seafoods
+    {
+      id: 7,
+      menuImage: Menu7,
+      menuTitle: 'Grilled Bangus',
+      menuCategory: 'Seafood',
+      menuPrice: '$460'
+    },
+    {
+      id: 8,
+      menuImage: Menu8,
+      menuTitle: 'Mixed Seafood',
+      menuCategory: 'Seafood',
+      menuPrice: '$520'
+    },
+    {
+      id: 9,
       menuImage: Menu9,
       menuTitle: 'Sweat & sour food',
       menuCategory: 'Seafood',
       menuPrice: '$560'
     },
     {
+      id: 10,
       menuImage: Menu10,
       menuTitle: 'Butter Garlic Shrimp',
       menuCategory: 'Seafood',
       menuPrice: '$540'
     },
-    { menuImage: Menu11, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$220' },
-    { menuImage: Menu12, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu13, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu14, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu15, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu16, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu17, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu18, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu19, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu20, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu21, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu22, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu23, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu24, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu25, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu26, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu27, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu28, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu29, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' },
-    { menuImage: Menu30, menuTitle: 'Calamares', menuCategory: 'Seafood', menuPrice: '$550' }
-  ]
-  const MenuButton = [
-    { holderButton: 'All' },
-    { holderButton: 'Meals' },
-    { holderButton: 'Seafood' },
-    { holderButton: 'Dessert' },
-    { holderButton: 'Sizzling' },
-    { holderButton: 'Beverages' },
-    { holderButton: 'Samgyupsal' },
-    { holderButton: 'Other' }
+    {
+      id: 11,
+      menuImage: Menu11,
+      menuTitle: 'Calamares',
+      menuCategory: 'Seafood',
+      menuPrice: '$220'
+    },
+    //Dessert
+    {
+      id: 12,
+      menuImage: Menu12,
+      menuTitle: 'Calamares',
+      menuCategory: 'Dessert',
+      menuPrice: '$550'
+    },
+    {
+      id: 13,
+      menuImage: Menu13,
+      menuTitle: 'Calamares',
+      menuCategory: 'Dessert',
+      menuPrice: '$550'
+    },
+    //Sizzling
+    {
+      id: 14,
+      menuImage: Menu14,
+      menuTitle: 'Calamares',
+      menuCategory: 'Sizzling',
+      menuPrice: '$550'
+    },
+    {
+      id: 15,
+      menuImage: Menu15,
+      menuTitle: 'Calamares',
+      menuCategory: 'Sizzling',
+      menuPrice: '$550'
+    },
+    {
+      id: 16,
+      menuImage: Menu16,
+      menuTitle: 'Calamares',
+      menuCategory: 'Sizzling',
+      menuPrice: '$550'
+    },
+    //Beverage
+    {
+      id: 17,
+      menuImage: Menu17,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    {
+      id: 18,
+      menuImage: Menu18,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    {
+      id: 19,
+      menuImage: Menu19,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    {
+      id: 20,
+      menuImage: Menu20,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    {
+      id: 21,
+      menuImage: Menu21,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    {
+      id: 22,
+      menuImage: Menu22,
+      menuTitle: 'Calamares',
+      menuCategory: 'Beverage',
+      menuPrice: '$550'
+    },
+    //Samgyup
+    {
+      id: 23,
+      menuImage: Menu23,
+      menuTitle: 'Calamares',
+      menuCategory: 'Samgyup',
+      menuPrice: '$550'
+    },
+    {
+      id: 24,
+      menuImage: Menu24,
+      menuTitle: 'Calamares',
+      menuCategory: 'Samgyup',
+      menuPrice: '$550'
+    },
+    //Add ons
+    {
+      id: 25,
+      menuImage: Menu25,
+      menuTitle: 'Calamares',
+      menuCategory: 'Add ons',
+      menuPrice: '$550'
+    },
+    {
+      id: 26,
+      menuImage: Menu26,
+      menuTitle: 'Calamares',
+      menuCategory: 'Add ons',
+      menuPrice: '$550'
+    },
+    {
+      id: 27,
+      menuImage: Menu27,
+      menuTitle: 'Calamares',
+      menuCategory: 'Add ons',
+      menuPrice: '$550'
+    },
+    {
+      id: 28,
+      menuImage: Menu28,
+      menuTitle: 'Calamares',
+      menuCategory: 'Add ons',
+      menuPrice: '$550'
+    },
+    {
+      id: 29,
+      menuImage: Menu29,
+      menuTitle: 'Halo Halo',
+      menuCategory: 'Add ons',
+      menuPrice: '$550'
+    },
+    { id: 30, menuImage: Menu30, menuTitle: 'Coke', menuCategory: 'Beverages', menuPrice: '$550' }
   ]
 
+  const uniqueCategories = ['All', ...new Set(MenuItems.map((item) => item.menuCategory))]
+
+  const MenuButton = uniqueCategories.map((category) => ({
+    holderButton: category
+  }))
+  // const [activeCategory, setActiveCategory] = useState('All')
+
+  // // ✅ Show button only if category exists in MenuItems
+  // const visibleButtons = MenuButton.filter((btn) => {
+  //   if (btn.holderButton === 'All') return true
+
+  //   return MenuItems.some((item) => item.menuCategory === btn.holderButton)
+  // })
+
+  // // ✅ Filter items based on selected category
+  // const filteredItems =
+  //   activeCategory === 'All'
+  //     ? MenuItems
+  //     : MenuItems.filter((item) => item.menuCategory === activeCategory)
+
   return (
-    <div className="h-full bg-[#F5F5F5] font-sans">
-      <main className="flex-1 flex flex-col min-h-0 ">
+    <div className="bg-[#F5F5F5] font-sans">
+      <main className="h-full flex flex-col w-full min-w-0">
         {/* Header */}
-        <div className="w-full grid grid-cols-2 ">
+        {/* <div className="w-full grid grid-cols-2"> */}
+        <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-3 mb-6">
           <div className="justify-item-end">
             <h1 className="text-3xl font-meduim text-black mb-1 xs:mb-2">Menu</h1>
             <p className="text-lg font-normal text-gray-600">See what's on your Menu</p>
           </div>
-          <div className="grid auto-cols-max grid-flow-col grid-row-3 gap-3 justify-self-end ">
-            <button class="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
+          <div className="flex flex-wrap grid auto-cols-max grid-flow-col gap-3 justify-self-end ">
+            <button className="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
               Take Out
             </button>
-            <button class="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
+            <button className="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
               Additional Order
             </button>
-            <button class="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
+            <button className="w-auto h-[42px] px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
               + Add New Order
             </button>
           </div>
         </div>
-        <div class=" w-full bg-white mt-4 p-4">
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 pl-3 sm:pl-4 flex items-center pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-400"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </div>
-            <input
-              type="text"
-              placeholder="Search menu"
-              className="w-full max-w-xs sm:max-w-sm md:max-w-md pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent text-base font-normal"
-            />
-          </div>
-          <div className="flex gap-6 my-3 overflow-x-scroll pb-4">
-            {MenuButton.map((itemB) => (
-              <button class="px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
-                {itemB.holderButton}
-              </button>
-            ))}
-          </div>
-          <div className="flex font-Meduim">
-            <h2 className="fex my-2">Meals</h2>
-          </div>
-          {/* <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-3 sm:gap-4"> */}
-          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-6  gap-3">
-            {MenuItems.map((item) => (
-              <div className="w-full h-[221px] p-2 m-1 border rounded-lg">
-                <img
-                  className="w-full h-[102px] border-10 rounded-xl border-gray-400"
-                  src={item.menuImage}
-                  alt={item.menuTitle}
-                />
-                <h1 className="text-black text-lg font-semibold">{item.menuTitle}</h1>
-                <p className="text-base font-normal text-gray-500">{item.menuCategory}</p>
-                <h2 className="text base font-semibold text-purple-600">{item.menuPrice}</h2>
+        <div className="flex-1 flex flex-col min-h-0 bg-white border border-gray-200 rounded-lg shadow-sm p-6">
+          <div className="flex flex-col gap-4 flex-1 min-h-0">
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 sm:pl-4 flex items-center pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
               </div>
-            ))}
-          </div>
-          {/* <div className="flex flex-row">
+              <input
+                type="text"
+                placeholder="Search menu"
+                className="w-full max-w-xs sm:max-w-sm md:max-w-md pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent text-base font-normal"
+              />
+            </div>
+            <div className="flex gap-6 my-3 overflow-x-auto pb-4">
+              {MenuButton.map((itemB) => (
+                <button className="px-4 py-2 font-meduim border border-purple-500 text-purple-900 bg-White-600 rounded-md ">
+                  {itemB.holderButton}
+                </button>
+              ))}
+            </div>
+            <div className="h-[50vh] overflow-y-auto ">
+              <div className="space-y-6 sm:space-y-8">
+                <div className="">
+                  <h2 className="text-black mb-3 sm:mb-4 font-semibold text-lg">Meals</h2>
+                </div>
+                {/* <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-3 sm:gap-4"> */}
+                {/* <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5  gap-3"> */}
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 sm:gap-4">
+                  {MenuItems.map((item) => (
+                    <div className="relative bg-white border rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-all flex flex-col">
+                      <img
+                        className="w-full h-[102px] border-10 rounded-xl border-gray-400"
+                        src={item.menuImage}
+                        alt={item.menuTitle}
+                      />
+                      <h1 className="text-black text-lg font-semibold">{item.menuTitle}</h1>
+                      <p className="text-base font-normal text-gray-500">{item.menuCategory}</p>
+                      <h2 className="text base font-semibold text-purple-600">{item.menuPrice}</h2>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="flex flex-row">
             {MenuItems.map((itemSeafood) => (
               <div className="flex flex-col w-[251px] h-[221px] p-2 m-1 border rounded-lg">
                 <img
@@ -170,6 +345,7 @@ const Menu = () => {
               </div>
             ))}
           </div> */}
+          </div>
         </div>
       </main>
     </div>
